ff0b16cbb1dcd2afaaba756fd627df415a82551c
Corentin Dupont <corentin.dupont@gmail.com>
2017-05-24 00:03:51 +0200

    My Modules button

--- src/Nomyx/Web/MainPage.hs
+++ src/Nomyx/Web/MainPage.hs
@@ -88,7 +88,9 @@ viewGamesTab gi isAdmin saveDir mpn = do
        tr $ td ! A.class_ "buttonTD" $ H.a "Home "         ! A.class_ "button" ! href (toValue $ showRelURL $ Menu Home gn)
        tr $ td ! A.class_ "buttonTD" $ H.a "Constitution " ! A.class_ "button" ! href (toValue $ showRelURL $ Menu Rules gn)
        tr $ td ! A.class_ "buttonTD" $ H.a "Actions "      ! A.class_ "button" ! href (toValue $ showRelURL $ Menu Actions gn)
-       tr $ td ! A.class_ "buttonTD" $ H.a "Library "      ! A.class_ "button" ! href (toValue $ showRelURL $ Menu Lib gn)
+     table ! A.class_ "libraryButtons" $ do
+       tr $ td ! A.class_ "buttonTD" $ H.a "My Rules "     ! A.class_ "button" ! href (toValue $ showRelURL $ Menu Lib gn)
+       tr $ td ! A.class_ "buttonTD" $ H.a "My Modules "   ! A.class_ "button" ! href (toValue $ showRelURL $ Menu Modules gn)
      -- br >> b "Help files:" >> br
      -- H.a "Nomyx language"    ! (href "/html/Language-Nomyx.html") ! target "_blank" >> br
 
